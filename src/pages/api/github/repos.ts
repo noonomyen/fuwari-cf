079443b9a69621ea8a054dcc7b14a29a2a057de7
noonomyen <nomyen@noonomyen.com>
2025-04-21 07:34:53 +0700

    change: check env.KV_GITHUB_API_CACHE and remove try block

--- src/pages/api/github/repos.ts
+++ src/pages/api/github/repos.ts
@@ -10,7 +10,8 @@ export const GET: APIRoute = async (context) => {
 
 	if (
 		!runtime.env.SECRET_GITHUB_API_CACHE_PAT ||
-		!runtime.env.SECRET_GITHUB_API_CACHE_SIG_KEY
+		!runtime.env.SECRET_GITHUB_API_CACHE_SIG_KEY ||
+		!runtime.env.KV_GITHUB_API_CACHE
 	) {
 		return new Response(null, { status: 503 });
 	}
@@ -39,78 +40,23 @@ export const GET: APIRoute = async (context) => {
 
 	const endpoint = `https://api.github.com/repos/${repository}`;
 
-	try {
-		const cacheResponse = await runtime.caches.default.match(endpoint);
+	const cacheResponse = await runtime.caches.default.match(endpoint);
 
-		if (cacheResponse) {
-			console.info("[/api/github/repos] Found in Cache");
+	if (cacheResponse) {
+		console.info("[/api/github/repos] Found in Cache");
 
-			return cacheResponse as unknown as Response;
-		}
+		return cacheResponse as unknown as Response;
+	}
 
-		console.info("[/api/github/repos] Not in Cache, trying to fetch from KV");
+	console.info("[/api/github/repos] Not in Cache, trying to fetch from KV");
 
-		const kvCacheResponse =
-			await runtime.env.KV_GITHUB_API_CACHE.get(repository);
+	const kvCacheResponse = await runtime.env.KV_GITHUB_API_CACHE.get(repository);
 
-		if (kvCacheResponse) {
-			console.info("[/api/github/repos] Found in KV");
-			console.info("[/api/github/repos] Caching response from KV");
+	if (kvCacheResponse) {
+		console.info("[/api/github/repos] Found in KV");
+		console.info("[/api/github/repos] Caching response from KV");
 
-			const response = new Response(kvCacheResponse, {
-				status: 200,
-				headers: {
-					"Content-Type": "application/json",
-					"Cache-Control": "public, max-age=3600, s-maxage=3600",
-					"Access-Control-Max-Age": "3600",
-				},
-			});
-
-			runtime.ctx.waitUntil(
-				runtime.caches.default.put(
-					endpoint,
-					response.clone() as unknown as CFResponse,
-				),
-			);
-
-			console.log(await runtime.caches.default.match(endpoint));
-
-			return response as unknown as Response;
-		}
-
-		console.info("[/api/github/repos] Not in KV, fetching from origin");
-
-		const githubResponse = await fetch(endpoint, {
-			headers: {
-				Authorization: `Bearer ${runtime.env.SECRET_GITHUB_API_CACHE_PAT}`,
-				Accept: "application/vnd.github+json",
-				"User-Agent": "fetch",
-			},
-		});
-
-		if (!githubResponse.ok) {
-			console.error(
-				`[/api/github/repos] Failed to fetch from origin, Status: ${githubResponse.status}`,
-			);
-
-			return new Response(null, { status: 503 });
-		}
-
-		const date = new Date();
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		const obj_data: { [key: string]: any } = await githubResponse.json();
-		const { permissions, ...filteredData } = obj_data;
-
-		filteredData._fetched_at = (date.getTime() / 1000) | 0;
-		const str_data = JSON.stringify(filteredData);
-
-		console.info("[/api/github/repos] Storing response from origin into KV");
-
-		runtime.env.KV_GITHUB_API_CACHE.put(repository, str_data);
-
-		console.info("[/api/github/repos] Caching response from origin");
-
-		const response = new Response(str_data, {
+		const response = new Response(kvCacheResponse, {
 			status: 200,
 			headers: {
 				"Content-Type": "application/json",
@@ -127,7 +73,55 @@ export const GET: APIRoute = async (context) => {
 		);
 
 		return response as unknown as Response;
-	} catch (err) {
-		return new Response(null, { status: 500 });
 	}
+
+	console.info("[/api/github/repos] Not in KV, fetching from origin");
+
+	const githubResponse = await fetch(endpoint, {
+		headers: {
+			Authorization: `Bearer ${runtime.env.SECRET_GITHUB_API_CACHE_PAT}`,
+			Accept: "application/vnd.github+json",
+			"User-Agent": "fetch",
+		},
+	});
+
+	if (!githubResponse.ok) {
+		console.error(
+			`[/api/github/repos] Failed to fetch from origin, Status: ${githubResponse.status}`,
+		);
+
+		return new Response(null, { status: 503 });
+	}
+
+	const date = new Date();
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const obj_data: { [key: string]: any } = await githubResponse.json();
+	const { permissions, ...filteredData } = obj_data;
+
+	filteredData._fetched_at = (date.getTime() / 1000) | 0;
+	const str_data = JSON.stringify(filteredData);
+
+	console.info("[/api/github/repos] Storing response from origin into KV");
+
+	runtime.env.KV_GITHUB_API_CACHE.put(repository, str_data);
+
+	console.info("[/api/github/repos] Caching response from origin");
+
+	const response = new Response(str_data, {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Cache-Control": "public, max-age=3600, s-maxage=3600",
+			"Access-Control-Max-Age": "3600",
+		},
+	});
+
+	runtime.ctx.waitUntil(
+		runtime.caches.default.put(
+			endpoint,
+			response.clone() as unknown as CFResponse,
+		),
+	);
+
+	return response as unknown as Response;
 };
